57a6e03f93f65f2fc7f00cecf46a4aca74804122
wonkothesaner <martin@wts.services>
2022-08-19 13:03:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-8ayuq3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-8ayuq3)
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  import { storyblokInit, apiPlugin } from "@storyblok/svelte";
+  import Feature from "../components/Feature.svelte";
+  import Grid from "../components/Grid.svelte";
+  import Page from "../components/Page.svelte";
+  import Teaser from "../components/Teaser.svelte";
+
+  storyblokInit({
+    accessToken: "1HabIcxFOnH2q9Mw0U1wPAtt",
+    use: [apiPlugin],
+    components: {
+      feature: Feature,
+      grid: Grid,
+      page: Page,
+      teaser: Teaser,
+    },
+  });
+</script>
+
+<script>
+  import "../app.css";
+</script>
+
+<main>
+  <slot />
+</main>
